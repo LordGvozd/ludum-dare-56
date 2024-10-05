79d1c77cf3f7522dfa4d4973e64b1388d10ba320
kr1tik <kritik97chel@gmail.com>
2024-10-05 22:16:11 +0600

    Tilemap

--- scripts/building/building_enter.gd
+++ scripts/building/building_enter.gd
@@ -1,13 +1,14 @@
 extends Node
 
 
-class_name HouseEnter
+class_name BuildingEnter
 
 
 @onready var load_scene_file_path: String = $"..".load_scene_file_path
 
 
-func _on_house_enter_area_body_entered(body):
+func _on_building_enter_area_body_entered(body):
 	if body is PlayerMovement:
 		if FileAccess.file_exists(load_scene_file_path):
-			get_tree().change_scene_to_file(load_scene_file_path)
+			get_tree().change_scene_to_file.bind(load_scene_file_path)\
+				.call_deferred()
