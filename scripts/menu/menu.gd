60ae9a42921e8790ad06b124f8b4d5b3856abf3c
LordGvozd <zehapo@yandex.ru>
2024-10-06 09:14:48 +0000

    Add volume settings

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -1,5 +1,14 @@
 extends Control
 
+@onready var buttons_container: MarginContainer = $"ButtonsContainer"
+@onready var settings_container: MarginContainer = $"CenterContainer"
+
+
+func _ready() -> void:
+	var slider_start_value = db_to_linear(AudioServer.get_bus_index("Master"))
+	
+	$"CenterContainer/VBoxContainer/HSlider".value = slider_start_value
+
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file.bind("res://scenes/town.tscn")\
@@ -8,3 +17,24 @@ func _on_play_pressed():
 	
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	buttons_container.visible = false
+	settings_container.visible = true
+	
+	$Button.visible = true
+	
+	
+		
+
+
+func _on_button_pressed() -> void:
+	buttons_container.visible = true
+	settings_container.visible = false
+	
+	$Button.visible = false
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
