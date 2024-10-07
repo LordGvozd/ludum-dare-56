c61048c2bfd7f169054f386f5f8b211f8d7013a9
LordGvozd <zehapo@yandex.ru>
2024-10-07 19:55:25 +0600

    BLEKA UBIVAET MENYA

--- scripts/building/town_enter.gd
+++ scripts/building/town_enter.gd
@@ -4,5 +4,6 @@ extends Node
 
 func _ready() -> void:
 	if SpawningData.town_spawn_position:
+		print("Kukuhoy edu")
 		player.player_movement.global_position = SpawningData.town_spawn_position
 		$AnimationPlayer.play("fade_out")
