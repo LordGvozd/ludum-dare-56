d45d988c0b34057b757fcb650981b667e8a4834f
LordGvozd <zehapo@yandex.ru>
2024-10-08 01:20:28 +0600

    Add final fade out

--- scripts/ending_area.gd
+++ scripts/ending_area.gd
@@ -1,8 +1,17 @@
 extends Area2D
 
 
+func _ready() -> void:
+	print($CanvasLayer/Control/ColorRect.visible)
+	$CanvasLayer/Control/ColorRect.visible = true
+
 func _on_body_entered(body):
 	if body is PlayerMovement:
+		$"AnimationPlayer".play("end")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "end":
 		get_tree().change_scene_to_file.bind("res://scenes/ending.tscn")\
-			.call_deferred()
+				.call_deferred()
 	
